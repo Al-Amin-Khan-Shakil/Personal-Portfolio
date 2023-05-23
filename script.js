a80ecb362dbbdf7cd28fd969fab6d3e99e1298a9
RICHARD-SIKAONGA <97953658+richie1988@users.noreply.github.com>
2023-05-23 17:41:06 +0200

    implemented javascript for mobile menu

---
+++ script.js
@@ -0,0 +1,9 @@
+//mobile menu
+function openNav() {
+    document.getElementById("navbar").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("navbar").style.width = "0%";
+}
+//mobile menu end
