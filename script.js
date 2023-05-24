9ac28cbd99d123b5b6c58059e4acb3800e5978d0
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-05-24 14:39:31 +0600

    Requried changes are made

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 /* mobile menu */
+const portfolioLink = document.getElementById('portfolio');
+const aboutMe = document.getElementById('about');
+const contactForm = document.getElementById('contact');
+
 function openNav() {
   document.getElementById('navbar').style.width = '100%';
 }
 function closeNav() {
   document.getElementById('navbar').style.width = '0%';
 }
+
+window.onclick = function removeMenu(event) {
+  switch (event.target) {
+    case portfolioLink:
+      document.getElementById('navbar').style.width = '0%';
+      break;
+    case aboutMe:
+      document.getElementById('navbar').style.width = '0%';
+      break;
+    case contactForm:
+      document.getElementById('navbar').style.width = '0%';
+      break;
+    default:
+  }
+};
 openNav();
 closeNav();
 /* mobile menu end */
